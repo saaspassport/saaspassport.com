7a0fd73889101ca6a50c2f4da0c68223a427e1a7
Kyle E. Mitchell <kyle@kemitchell.com>
2021-10-02 16:52:35 -0700

    Set Twitter handle

--- constants.js
+++ constants.js
@@ -3,6 +3,7 @@
 export default {
   website: 'SaaS Passport',
   slogan: 'standard software as a service terms',
+  twitter: 'saaspassport',
   support: 'support@saaspassport.com',
   base: 'https://saaspassport.com',
   cookie: 'saaspassport'
